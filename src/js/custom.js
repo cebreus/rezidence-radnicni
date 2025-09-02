9c05819c5cc7b4e1f995c4457fd786bd534eb9e2
Pavel Babacek <pavel@babacek.cz>
2025-09-02 17:20:07 +0200

    feat: images, table and header

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,1 +1,23 @@
-console.log('Source file `custom.js` processed.');
+// console.log('Source file `custom.js` processed.');
+
+const navbar = document.querySelector('.o-header');
+window.onscroll = () => {
+  if (window.scrollY > 10) {
+    navbar.classList.add('scroll');
+  } else {
+    navbar.classList.remove('scroll');
+  }
+};
+
+document.querySelectorAll('.offcanvas-body .nav-link').forEach((item) => {
+  item.addEventListener('click', (event) => {
+    event.preventDefault();
+    const href = item.getAttribute('href');
+    const { offsetTop } = document.querySelector(href);
+    window.scrollTo({
+      top: offsetTop,
+      behavior: 'smooth',
+    });
+    document.querySelector('#offcanvasNavbar2').classList.remove('show');
+  });
+});
